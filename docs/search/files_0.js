57dc400cc8861b0ac5586a063661984b4727707c
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-07-22 18:45:55 +0300

    Use doxygen-mcss for better looking documentation.

--- docs/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['softrender_2ecpp',['softrender.cpp',['../softrender_8cpp.html',1,'']]],
-  ['softrender_2ehpp',['softrender.hpp',['../softrender_8hpp.html',1,'']]]
-];
--- docs/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sdl_5ferror',['sdl_error',['../softrender_8cpp.html#ae9850a581a421ca5b33e2a11afe9a3b3',1,'softrender.cpp']]]
-];
