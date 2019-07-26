cdffe18a12d8ef7cf3d6cb6fe6eecd76c14f594b
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-07-26 11:31:53 +0300

    Test the draw_point_function.

---
+++ tests/point.cpp
@@ -0,0 +1,20 @@
+#include "softrender.hpp"
+
+int main(int, char*[])
+{
+    using namespace softrender;
+    window_t window{1280, 720};
+
+    while(!window.closed()) {
+        // clang-format off
+        for(int i = window.width() / 2 - 100;
+                i <= window.width() / 2 + 100;
+                ++i)
+        // clang-format on
+        {
+            window.draw_point(window.height() / 2, i, pixel_t{255, 255, 255});
+        }
+        window.draw();
+    }
+}
+
