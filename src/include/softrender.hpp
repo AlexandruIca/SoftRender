0ec5b2a4aa0f8133ac14a954af5768982c7a4cfd
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-07-26 10:55:08 +0300

    Provide the most basic functionality for getting a window shown.
    Currently only implementing ways to:
    - create a window
    - draw to the window
    - check whether a window has been closed(by being closed by the user
    explicitly or by pressing ESCAPE
    
    Soon there should be functionality for event callbacks and drawing a
    point.

--- src/include/softrender.hpp
+++ src/include/softrender.hpp
@@ -1,7 +1,110 @@
 ///
 /// \file
 ///
-/// Contains a function that check if sdl was set up correctly.
+
+#include <cstddef>
+#include <vector>
+
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
+
 ///
+/// \brief All key functionality is in this namespace.
+///
+namespace softrender {
+
+///
+/// \brief Struct to represent an rgba value.
+///
+struct pixel_t
+{
+    std::byte r = std::byte{0};
+    std::byte g = std::byte{0};
+    std::byte b = std::byte{0};
+    std::byte a = std::byte{255};
+};
+
+///
+/// \brief Use this to construct a window.
+///
+/// This is just a wrapper around SDL to construct a surface from memory and
+/// blit it.
+///
+/// \note There is no separate renderer class to keep the code super simple.
+///
+class window_t
+{
+private:
+    std::vector<pixel_t> m_canvas;
+
+    int m_width;
+    int m_height;
+    bool m_running{true};
+
+    SDL_Window* m_window{nullptr};
+    SDL_Renderer* m_renderer{nullptr};
+    SDL_Texture* m_texture{nullptr};
+
+    void construct_canvas();
+    void initialize_sdl();
+
+public:
+    ///
+    /// \brief Constructs a window with width=640, height=480.
+    ///
+    window_t();
+    ///
+    /// \brief Constructs a window with \ref t_width width and t_height
+    ///        height.
+    ///
+    window_t(int const t_width, int const t_height);
+    ~window_t() noexcept;
+
+    ///
+    /// \brief Get width of constructed window.
+    ///
+    int width() const noexcept;
+    ///
+    /// \brief Get height of constructed window.
+    ///
+    int height() const noexcept;
+
+    ///
+    /// \brief Draws everything to the screen and handles input.
+    ///
+    /// \note There are no separate functions (yet) to retreive events
+    ///       to keep the code simple, but when the need arises they will
+    ///       be added to the class. Currently, if you press ESCAPE the
+    ///       application exits.
+    ///
+    void draw();
+
+    ///
+    /// \brief Returns true if the window will close.
+    ///
+    /// \note If you press ESCAPE the application terminates.
+    ///
+    bool closed() const noexcept;
+
+    ///
+    /// \brief Access the rgba value at [line](\ref t_i) and [column](\ref t_j).
+    ///
+    /// \param t_i Line of the inner rgba matrix.
+    /// \param t_j Column of the inner rgba matrix.
+    ///
+    pixel_t& operator()(int const t_i, int const t_j);
+    ///
+    /// \brief Get a const reference to the rgba value at [line](\ref t_i) and
+    ///        [column](\ref t_j).
+    ///
+    /// \param t_i Line of the inner rgba matrix.
+    /// \param t_j Column of the inner rgba matrix.
+    ///
+    pixel_t const& operator()(int const t_i, int const t_j) const;
+};
+
+} // namespace softrender
+
 int test();
 
--- src/softrender.cpp
+++ src/softrender.cpp
@@ -1,19 +1,18 @@
 ///
 /// \file
 ///
-
 #include "softrender.hpp"
-
 #include "SDL.h"
 
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 ///
 /// \brief Wrapper around **SDL_GetError**.
 ///
-void sdl_error(std::string const& t_error)
+static void sdl_error(std::string const& t_error)
 {
     std::cout << "Error: " << t_error << "\n\t";
     std::cout << "SDL: " << SDL_GetError() << std::endl << std::endl;
@@ -32,4 +31,176 @@ int test()
 
     return EXIT_SUCCESS;
 }
+
+namespace softrender {
+
+void error(std::string const& t_error)
+{
+    sdl_error(t_error);
+#ifdef SOFTRENDER_DEBUG
+    throw std::runtime_error(t_error);
+#endif
+}
+
+void window_t::initialize_sdl()
+{
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        error("Could not initialize sdl!");
+    }
+
+    m_window = SDL_CreateWindow("SoftRender", SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED, m_width, m_height,
+                                SDL_WINDOW_SHOWN);
+
+    if(m_window == nullptr) {
+        error("Could not create window!");
+    }
+
+    SDL_SetWindowResizable(m_window, SDL_FALSE);
+
+    m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
+
+    if(m_renderer == nullptr) {
+        error("Could not create a renderer!");
+    }
+}
+
+void window_t::construct_canvas()
+{
+    m_canvas.resize(m_width * m_height);
+
+    for(int i = 0; i < m_height; ++i) {
+        for(int j = 0; j < m_width; ++j) {
+            this->operator()(i, j) = pixel_t{};
+        }
+    }
+}
+
+window_t::window_t()
+    : m_width{640}
+    , m_height{480}
+{
+    this->initialize_sdl();
+    this->construct_canvas();
+}
+
+window_t::window_t(int const t_width, int const t_height)
+    : m_width{t_width}
+    , m_height{t_height}
+{
+    this->initialize_sdl();
+    this->construct_canvas();
+}
+
+window_t::~window_t() noexcept
+{
+    if(m_texture != nullptr) {
+        SDL_DestroyTexture(m_texture);
+    }
+    SDL_DestroyRenderer(m_renderer);
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
+}
+
+int window_t::width() const noexcept
+{
+    return m_width;
+}
+
+int window_t::height() const noexcept
+{
+    return m_height;
+}
+
+namespace surface_info {
+
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+constexpr Uint32 rmask = 0xff000000;
+constexpr Uint32 gmask = 0x00ff0000;
+constexpr Uint32 bmask = 0x0000ff00;
+constexpr Uint32 amask = 0x000000ff;
+#else
+constexpr Uint32 rmask = 0x000000ff;
+constexpr Uint32 gmask = 0x0000ff00;
+constexpr Uint32 bmask = 0x00ff0000;
+constexpr Uint32 amask = 0xff000000;
+#endif
+
+} // namespace surface_info
+
+void window_t::draw()
+{
+    if(m_texture != nullptr) {
+        SDL_DestroyTexture(m_texture);
+    }
+
+    using namespace surface_info;
+
+    SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(
+        reinterpret_cast<void*>(m_canvas.data()), m_width, m_height, 32,
+        4 * m_width, rmask, gmask, bmask, amask);
+
+    if(surface == nullptr) {
+        error("Could not create surface to draw!");
+    }
+
+    m_texture = SDL_CreateTextureFromSurface(m_renderer, surface);
+
+    SDL_Rect screen;
+    screen.x = 0;
+    screen.y = 0;
+    screen.w = m_width;
+    screen.h = m_height;
+
+    SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
+    SDL_RenderClear(m_renderer);
+    SDL_RenderCopy(m_renderer, m_texture, nullptr, &screen);
+    SDL_RenderPresent(m_renderer);
+    SDL_FreeSurface(surface);
+
+    SDL_Event e;
+    while(SDL_PollEvent(&e)) {
+        switch(e.type) {
+        case SDL_QUIT:
+            m_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if(e.key.keysym.sym == SDLK_ESCAPE) {
+                m_running = false;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+bool window_t::closed() const noexcept
+{
+    return !m_running;
+}
+
+pixel_t& window_t::operator()(int const t_i, int const t_j)
+{
+#ifdef SOFTRENDER_DEBUG
+    if(t_i * m_width + t_j >= m_width * m_height) {
+        throw std::runtime_error("Canvas coordinates out of bounds.");
+    }
+#endif
+
+    return m_canvas[t_i * m_width + t_j];
+}
+
+pixel_t const& window_t::operator()(int const t_i, int const t_j) const
+{
+#ifdef SOFTRENDER_DEBUG
+    if(t_i * m_width + t_j >= m_width * m_height) {
+        throw std::runtime_error("Canvas coordinates out of bounds.");
+    }
+#endif
+
+    return m_canvas[t_i * m_width + t_j];
+}
+
+} // namespace softrender
 
