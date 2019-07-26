c83c440090b2e6bb2073a229918c65bc86711cd0
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-07-26 11:50:54 +0300

    Make sure exceptions are caught for invalid input.

---
+++ tests/exception.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "softrender.hpp"
+
+int main()
+{
+    try {
+        using namespace softrender;
+        window_t window;
+
+        while(!window.closed()) {
+            window.draw_point(1000, 1000, pixel_t{0, 0, 0, 0});
+            window.draw();
+        }
+    }
+    catch(std::exception const& e) {
+        std::cout << "Exception caught!" << std::endl << '\t';
+        std::cout << e.what() << std::endl;
+    }
+    catch(...) {
+        std::cout << "Uncaught exception!!!" << std::endl;
+    }
+}
+
