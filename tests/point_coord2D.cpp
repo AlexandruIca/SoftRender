cea2f70f4f247b91b094a01352ccfe41aba69e84
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-07-29 18:39:57 +0300

    Test new point_t struct.

---
+++ tests/point_coord2D.cpp
@@ -0,0 +1,21 @@
+#include "softrender.hpp"
+
+#include <iostream>
+
+int main(int, char*[])
+{
+    using namespace softrender;
+    window_t window{1280, 720};
+
+    while(!window.closed()) {
+        window.draw_point({0, 0}, {0, 255, 0});
+        window.draw_point({0, window.height() - 1}, {255, 255, 0});
+        window.draw_point({window.width() - 1, 0}, {255, 255, 255});
+        window.draw_point({window.width() - 1, window.height() - 1},
+                          {255, 0, 0});
+        window.draw_point({window.width() / 2, window.height() / 2},
+                          {122, 122, 122});
+        window.draw();
+    }
+}
+
