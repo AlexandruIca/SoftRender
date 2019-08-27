c09e962e2b8fd5bcd0966dbf9e80c5c69ee1ae2f
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-08-27 19:08:51 +0300

    Test new 'canvas manipulation' functionality.

---
+++ tests/canvas_manipulation.cpp
@@ -0,0 +1,13 @@
+#include "softrender.hpp"
+
+int main(int, char*[])
+{
+    using namespace softrender;
+    window_t window{ 1280, 720 };
+
+    while(!window.closed()) {
+        window.canvas()[window.from_coord2d_to_matrix(point_t{
+            window.width() / 2, window.height() / 2 })] = pixel_t{ 255, 0, 0 };
+        window.draw();
+    }
+}
