f3637854f83c0f1915edb5f484bd19466de631b1
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-09-03 12:28:47 +0300

    Make lines more visible and switch the order of points for more extensive testing.

--- tests/line.cpp
+++ tests/line.cpp
@@ -7,10 +7,10 @@ int main(int, char*[])
 
     while(!window.closed()) {
         window.draw_line({ 30, 30 }, { 400, 400 }, { 255, 169, 47 });
-        window.draw_line({ 30, 400 }, { 400, 30 }, { 68, 23, 23 });
-        window.draw_line({ window.width() / 2 - 100, window.height() / 2 },
-                         { window.width() / 2 + 100, window.height() / 2 },
-                         pixel_t{});
+        window.draw_line({ 30, 400 }, { 400, 30 }, { 168, 123, 123 });
+        window.draw_line({ window.width() / 2 + 100, window.height() / 2 },
+                         { window.width() / 2 - 100, window.height() / 2 },
+                         pixel_t{ 255, 255, 255 });
         window.draw();
     }
 }
