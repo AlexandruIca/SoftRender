02d9913a72c214a325149b55f4d136861e0c58bc
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-09-04 17:18:36 +0300

    Test new predefined colors.

---
+++ tests/colors.cpp
@@ -0,0 +1,26 @@
+#include <array>
+
+#include "softrender.hpp"
+
+int main(int, char*[])
+{
+    using namespace softrender;
+
+    std::array color = { colors::amber,  colors::black,  colors::blue,
+                         colors::brown,  colors::cyan,   colors::green,
+                         colors::grey,   colors::indigo, colors::lime,
+                         colors::orange, colors::pink,   colors::purple,
+                         colors::red,    colors::teal,   colors::white,
+                         colors::yellow };
+
+    window_t window{ 1280, 720 };
+
+    while(!window.closed()) {
+        for(int i = 0; i < color.size(); ++i) {
+            window.draw_line({ window.width() / 2 - 100, (i + 1) * 20 },
+                             { window.width() / 2 + 100, (i + 1) * 20 },
+                             color[i]);
+        }
+        window.draw();
+    }
+}
