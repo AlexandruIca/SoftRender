bd8e69fc8df83ac4e463588ceac34871a1473c5b
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-09-17 17:45:43 +0300

    Run gcov in each directory.

--- scripts/codecov-io-azure.sh
+++ scripts/codecov-io-azure.sh
@@ -1,13 +1,19 @@
 for dir in src/CMakeFiles/*.dir; do
-    gcov-9 $dir/*.gcno
+    pushd $dir
+    gcov-9 *.gcno
+    popd
 done
 
-for dir2 in tests/CMakeFiles/*.dir/*.gcno; do
-    gcov-9 $dir2
+for dir2 in tests/CMakeFiles/*.dir; do
+    pushd $dir2
+    gcov-9 *.gcno
+    popd
 done
 
-for dir3 in tests/basic_wire_render/CMakeFiles/*.dir/*.gcno; do
-    gcov-9 $dir3
+for dir3 in tests/basic_wire_render/CMakeFiles/*.dir; do
+    pushd $dir3
+    gcov-9 *.gcno
+    popd
 done
 
 bash <(curl -s https://codecov.io/bash)
