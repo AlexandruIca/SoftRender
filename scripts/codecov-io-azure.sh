e1d69f95eadd79ebe2570dc6d94e410841c80f32
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-09-17 17:28:51 +0300

    Fix incorrect path to code coverage.

--- scripts/codecov-io-azure.sh
+++ scripts/codecov-io-azure.sh
@@ -1,9 +1,13 @@
 for dir in src/CMakeFiles/*.dir; do
-    gcov-9 $dir/src/*.gcno
+    gcov-9 $dir/*.gcno
 done
 
 for dir2 in tests/CMakeFiles/*.dir/*.gcno; do
     gcov-9 $dir2
+done
+
+for dir3 in tests/basic_wire_render/CMakeFiles/*.dir/*.gcno; do
+    gcov-9 $dir3
 done
 
 bash <(curl -s https://codecov.io/bash)
