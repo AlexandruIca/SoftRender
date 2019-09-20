89698a559f586b63809eae1bbea7858fa4de29aa
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-09-20 12:49:32 +0300

    Prepare for next release.

--- src/include/docs.hpp
+++ src/include/docs.hpp
@@ -9,11 +9,11 @@
 ///
 /// To get started using the library you first have to download it for your
 /// platform(
-/// [MacOS](https://github.com/AlexandruIca/SoftRender/releases/download/nightly/SoftRender-0.2-Darwin.tar.gz
+/// [MacOS](https://github.com/AlexandruIca/SoftRender/releases/download/nightly/SoftRender-0.3-Darwin.tar.gz
 /// ),
-/// [Windows](https://github.com/AlexandruIca/SoftRender/releases/download/nightly/SoftRender-0.2-win64.zip
+/// [Windows](https://github.com/AlexandruIca/SoftRender/releases/download/nightly/SoftRender-0.3-win64.zip
 /// ), or
-/// [Linux](https://github.com/AlexandruIca/SoftRender/releases/download/nightly/SoftRender-0.2-Linux.tar.gz
+/// [Linux](https://github.com/AlexandruIca/SoftRender/releases/download/nightly/SoftRender-0.3-Linux.tar.gz
 /// )).
 /// After that you have to add the \p include folder to your compiler's path and
 /// the \p lib folder to your compiler's link path and link with either the
