c6858ecedc56a132c52b33355cfee307bb41f657
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-09-20 12:08:43 +0300

    Fix formatting [clang-format].

--- tests/key_callback.cpp
+++ tests/key_callback.cpp
@@ -7,7 +7,7 @@ auto main(int, char*[]) noexcept -> int
     softrender::window_t window{ 1280, 720 };
 
     window.set_on_key_press_callback(
-        [](softrender::key_event_t const& t_key_event) noexcept->void {
+        [](softrender::key_event_t const& t_key_event) noexcept -> void {
             std::cout << "Key code: " << t_key_event.keyc << std::endl;
             std::cout << "Repeating: " << t_key_event.repeat << std::endl;
             std::cout << "Released: " << t_key_event.released << std::endl;
