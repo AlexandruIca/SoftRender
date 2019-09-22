82adeba95b195d20110acc525b4ed66d41783085
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2019-09-22 20:27:57 +0300

    Lay the foundation for examples/ directory.

---
+++ examples/simple_snake/main.cpp
@@ -0,0 +1,13 @@
+#include "softrender.hpp"
+
+auto main(int, char*[]) -> int
+{
+    using namespace softrender;
+    window_t window{ 1280, 720 };
+
+    while(!window.closed()) {
+        window.draw();
+    }
+
+    return 0;
+}
